9701a687284a4d233bf6dc5d3adaf53b2e936c8a
Mark Sigel <msigel@wustl.edu>
2021-04-26 15:14:23 -0500

    created clothing suggestion class

---
+++ src/project/ClothingSuggestion.java
@@ -0,0 +1,82 @@
+package project;
+
+public class ClothingSuggestion {
+
+	public ClothingSuggestion() {}
+	
+	/**
+	 * Gets the clothing suggestion for daily weather forecast
+	 * 
+	 * @author mark
+	 * @param temp - the temperature for the days forecast
+	 * @param state - the weather state abbreviation
+	 * @return - the clothing suggestion
+	 */
+	public static String getClothingSuggestion(String temp, String state) {
+		
+		if(!state.equals("hc") && !state.equals("lc") && !state.equals("c")) {
+			return getStateSuggestion(state);
+		}
+		
+		return getTempSuggestion(temp);
+	}
+	
+	/**
+	 * Gets the clothing suggestion for daily weather forecast based on temp
+	 * 
+	 * @author mark
+	 * @param temp - the temperature for the days forecast
+	 * @return - the clothing suggestion
+	 */
+	public static String getTempSuggestion(String temp) {
+		if(temp == null || temp.equals("")) {
+			return "cannot get clothing suggestion";
+		}
+		
+		double tempNum =  Double.parseDouble(temp);
+		
+		if (tempNum >= 25) {
+			return "It's hot! Try wearing shorts and T-shirt.";
+		} else if (tempNum < 25 && tempNum >= 20) {
+			return "It's warm. Try wearing a T-shirt with shorts or pants.";
+		} else if (tempNum < 20 && tempNum >= 15) {
+			return "Try wearing pants and a lightweight jacket.";
+		} else if (tempNum < 15 && tempNum >= 10) {
+			return "It's cool.  Wear pants and a jacket.";
+		} else if (tempNum < 10 && tempNum >= 0) {
+			return "It's cold.  You should wear warm jacket.";
+		} else {
+			return "It's freezing!.  Make sure to wear a winter jacket.";
+		}
+	}
+	
+	/**
+	 * Gets the clothing suggestion for daily weather forecast
+	 * 
+	 * @author mark
+	 * @param state - the weather state abbv
+	 * @return - the clothing suggestion
+	 */
+	public static String getStateSuggestion(String state) {
+		if(state == null) {
+			return "unable to get suggestion";
+		}
+		
+		String[] rainSuggests  = {"Bring an umbrella!", "Wear a raincoat!"};
+		String[] snowSuggests = {"Wear a hat and gloves!", "Wear winter boots!"};
+		
+		switch(state) {
+		case "t":
+		case "hr":
+		case "lr":
+		case "s":
+			return rainSuggests[(int)(Math.random()*(rainSuggests.length))];
+		case "sn":
+		case "sl":
+		case "h":
+			return snowSuggests[(int)(Math.random()*(snowSuggests.length))];
+		default:
+			return "unable to get suggestion";
+		}
+	}
+}
--- src/project/UserInterface.java
+++ src/project/UserInterface.java
@@ -18,7 +18,7 @@ import project.WeatherAPIInterface.ForecastContainer;
  */
 public class UserInterface {
 
-	Scanner scanner = new Scanner(System.in);
+	ClothingSuggestion data = new ClothingSuggestion(new Scanner(System.in));
 
 	public UserInterface() {
 	}
@@ -66,7 +66,7 @@ public class UserInterface {
 		present(prompt);
 		System.out.println("");
 		String input = null;
-		input = this.scanner.nextLine();
+		input = this.data.scanner.nextLine();
 		return input;
 	}
 
@@ -97,7 +97,7 @@ public class UserInterface {
 
 		// Get user input
 
-		String input = this.scanner.nextLine();
+		String input = this.data.scanner.nextLine();
 		try {
 			response = Integer.parseInt(input.trim()) - 1;
 		} catch (NumberFormatException e) {
@@ -211,7 +211,7 @@ public class UserInterface {
 				this.outputDateTime(container);
 				System.out.println("\tTemp: " + formatTemp(f.the_temp));
 				outputWeatherConditions(f);
-				System.out.println("\tClothing Suggestion: " + this.getClothingSuggestion(f.the_temp, f.weather_state_abbr));
+				System.out.println("\tClothing Suggestion: " + ClothingSuggestion.getClothingSuggestion(f.the_temp, f.weather_state_abbr));
 				notFound = false;
 				break;
 			}
@@ -254,81 +254,6 @@ public class UserInterface {
 		return ret;
 	}
 	
-	/**
-	 * Gets the clothing suggestion for daily weather forecast
-	 * 
-	 * @author mark
-	 * @param temp - the temperature for the days forecast
-	 * @param state - the weather state abbreviation
-	 * @return - the clothing suggestion
-	 */
-	public String getClothingSuggestion(String temp, String state) {
-		
-		if(!state.equals("hc") && !state.equals("lc") && !state.equals("c")) {
-			return getStateSuggestion(state);
-		}
-		
-		return getTempSuggestion(temp);
-	}
-	
-	/**
-	 * Gets the clothing suggestion for daily weather forecast based on temp
-	 * 
-	 * @author mark
-	 * @param temp - the temperature for the days forecast
-	 * @return - the clothing suggestion
-	 */
-	public String getTempSuggestion(String temp) {
-		if(temp == null || temp.equals("")) {
-			return "cannot get clothing suggestion";
-		}
-		
-		double tempNum =  Double.parseDouble(temp);
-		
-		if (tempNum >= 25) {
-			return "It's hot! Try wearing shorts and T-shirt.";
-		} else if (tempNum < 25 && tempNum >= 20) {
-			return "It's warm. Try wearing a T-shirt with shorts or pants.";
-		} else if (tempNum < 20 && tempNum >= 15) {
-			return "Try wearing pants and a lightweight jacket.";
-		} else if (tempNum < 15 && tempNum >= 10) {
-			return "It's cool.  Wear pants and a jacket.";
-		} else if (tempNum < 10 && tempNum >= 0) {
-			return "It's cold.  You should wear warm jacket.";
-		} else {
-			return "It's freezing!.  Make sure to wear a winter jacket.";
-		}
-	}
-	
-	/**
-	 * Gets the clothing suggestion for daily weather forecast
-	 * 
-	 * @author mark
-	 * @param state - the weather state abbv
-	 * @return - the clothing suggestion
-	 */
-	public String getStateSuggestion(String state) {
-		if(state == null) {
-			return "unable to get suggestion";
-		}
-		
-		String[] rainSuggests  = {"Bring an umbrella!", "Wear a raincoat!"};
-		String[] snowSuggests = {"Wear a hat and gloves!", "Wear winter boots!"};
-		
-		switch(state) {
-		case "t":
-		case "hr":
-		case "lr":
-		case "s":
-			return rainSuggests[(int)(Math.random()*(rainSuggests.length))];
-		case "sn":
-		case "sl":
-		case "h":
-			return snowSuggests[(int)(Math.random()*(snowSuggests.length))];
-		default:
-			return "unable to get suggestion";
-		}
-	}
 	/**
 	 *Displays weather condition information such as wind, humdiity, air pressure, visibility, and weather
 	 *@author Clay
